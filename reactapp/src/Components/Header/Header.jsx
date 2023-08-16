f7c5a427bbe4242c1af330740592ae66fa967c32
askpatrik <ask.patrik@hotmail.com>
2023-08-16 14:16:18 +0200

    Add project files.

---
+++ reactapp/src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function App() {
+    return (
+        <div className="container">
+            <nav id="navbar" className="navbar navbar-expand-sm">
+                <h1>Header exempel</h1>
+            </nav>
+          
+            
+        </div>
+    )
+}
+export default App; 
---
+++ reactapp/src/Components/Test/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Test.css';
+
+function App() {
+    return (
+        <div className="container">
+            <h1>Hello, World</h1>
+            <p>I am writing JSX</p>
+        </div>
+    )
+}
+export default App; 
