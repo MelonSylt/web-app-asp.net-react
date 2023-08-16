87053f77f27d1ea54a58d5c9fc1f9da3396a693c
MelonSylt <elia.at@hotmail.com>
2023-08-16 14:36:54 +0200

    2:36 push

--- reactapp/src/Components/Test/Test.jsx
+++ reactapp/src/Components/Test/Test.jsx
@@ -8,6 +8,7 @@ function App() {
             <h1>Lets change this!!!</h1>
             <p>I am writing JSX</p>
             <h2>Test NUMBER 222222</h2>
+            <h1>okej en bra test 2:36</h1>
         </div>
     )
 }
