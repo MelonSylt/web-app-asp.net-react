bb42a1fcd4c614c0d02dee2da505f54f48fe2756
askpatrik <ask.patrik@hotmail.com>
2023-08-16 14:30:59 +0200

    Nytt skit

--- reactapp/src/Components/Test/Test.jsx
+++ reactapp/src/Components/Test/Test.jsx
@@ -4,7 +4,7 @@ import './Test.css';
 function App() {
     return (
         <div className="container">
-            <h1>Hello, World</h1>
+            <h1>Hello, NYTT TEST kl 14:30</h1>
             <h1>Lets change this!!!</h1>
             <p>I am writing JSX</p>
         </div>
