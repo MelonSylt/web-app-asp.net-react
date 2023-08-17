ce8fb6d19835e1ff9ab6d1be96f7744754668a86
askpatrik <ask.patrik@hotmail.com>
2023-08-17 21:33:37 +0200

    Bootstrap installerat och kan nu importeras i components

---
+++ reactapp/src/Components/Body/Body.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function MyComponent() {
+    return <div className="container">
+        <h1>Hello, Bootstrap!</h1>
+        <button className="btn btn-primary">Click me</button>
+    </div>
+
+}
--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Header from './Components/Header/Header.jsx'
 import './index.css'
+import Body from './Components/Body/Body.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
   <Header />
         <App />
-        
+        <Body />
 
         
 
