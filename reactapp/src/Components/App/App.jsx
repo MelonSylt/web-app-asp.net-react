9b1e8e69cad093c914841d5cfbab5980806bfcb1
askpatrik <ask.patrik@hotmail.com>
2023-08-18 11:10:22 +0200

    More card desgin

--- reactapp/src/Components/App/App.jsx
+++ reactapp/src/Components/App/App.jsx
@@ -12,32 +12,43 @@ export default class App extends Component {
         this.populateArticleData();
     }
 
-  
+    renderArticles() {
+        return (
+            <div className="row">
+            <div className="col-md-8">
+                <div className="row">
+                    {this.state.articles.map(article => (
+                        <Card
+                            key={article.title}
+                            title={article.title}
+                            link={article.link}
+                            published={article.published}
+                            summary={article.summary}
+                        />
+                    ))}
+                </div>
+                </div>
+                <div className="col-4">
+                    {this.state.articles.map(article => (
+                        <Card
+                            key={article.title}
+                            title={article.title}
+                            link={article.link}
+                            published={article.published}
+                            summary={article.summary}
+                        />
+                    ))}
+                </div>
+            </div>
+        );
+    }
 
     render() {
-        let contents = this.state.loading
-            ? <p><em>Loading... Please refresh once the ASP.NET backend has started.</em></p>
-            : (
-                <div className="col-md-8">
-                    <div className="row">
-                        {this.state.articles.map(article => (
-                            <Card
-                                key={article.title}
-                                title={article.title}
-                                link={article.link}
-                                published={article.published}
-                                summary={article.summary}
-                            />
-                        ))}
-                    </div>
-                </div>
-            );
-
         return (
             <div>
-                <h1 id="tabelLabel">Article List</h1>
-                <p>This component demonstrates fetching data from the server.</p>
-                {contents}
+                {this.state.loading
+                    ? <p><em>Loading... Please refresh once the ASP.NET backend has started.</em></p>
+                    : this.renderArticles()}
             </div>
         );
     }
--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -4,7 +4,6 @@ import App from './Components/App/App.jsx'
 import Header from './Components/Header/Header.jsx'
 import './index.css'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
-import Body from './Components/Body/Body.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
